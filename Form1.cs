949cae8c065aedbf8f47961485d1ec5e0ad5f6a4
Ron Enubs <ronenubs@gmail.com>
2020-08-17 13:17:05 +0800

    Revert "put code in button2"

--- Form1.cs
+++ Form1.cs
@@ -16,10 +16,5 @@ namespace WindowsFormsApp11
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
